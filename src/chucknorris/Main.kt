f50909fca94bddd560115e08cfac91530df6bbde
Pavlo Zhukov <pahazhukov@gmail.com>
2022-12-11 17:52:39 +0100

    feat: CLI API with exception handling

--- src/chucknorris/Main.kt
+++ src/chucknorris/Main.kt
@@ -1,11 +1,34 @@
 package chucknorris
 
+import chucknorris.utils.ChuckNorrisEncodeException
 import chucknorris.utils.chuckNorrisToString
+import chucknorris.utils.stringToChuckNorris
 
 fun main() {
-    println("Input string:")
-    val input = readln().trim()
-    val result = chuckNorrisToString(input)
-    println("The result:")
-    println(result)
+    while (true) {
+        println("Please input operation (encode/decode/exit):")
+        when (val operation = readln().trim()) {
+            "encode" -> {
+                println("Input string:")
+                val input = readln().trim()
+                val result = stringToChuckNorris(input)
+                println("Encoded string:")
+                println(result)
+            }
+            "decode" -> {
+                println("Input encoded string:")
+                val input = readln().trim()
+                try {
+                    val result = chuckNorrisToString(input)
+                    println("Decoded string:")
+                    println(result)
+                } catch (e: ChuckNorrisEncodeException) {
+                    println(e.message)
+                }
+            }
+            "exit" -> break
+            else -> println("There is no '$operation' operation")
+        }
+    }
+    println("Bye!")
 }
--- src/chucknorris/utils/chuck-norris.kt
+++ src/chucknorris/utils/chuck-norris.kt
@@ -1,10 +1,16 @@
 package chucknorris.utils
 
+private const val BINARY_SEQUENCE_LENGTH = 7
+
+class ChuckNorrisEncodeException : RuntimeException("Encoded string is not valid.")
+
 private fun toChuckNorrisSequence(char: Char, count: Int): String {
     val prefix = when (char) {
         '0' -> "00"
         '1' -> "0"
-        else -> { throw Exception("Unknown character: $char.") }
+        else -> {
+            throw Exception("Unknown character: $char.")
+        }
     }
     return "$prefix ${"0".repeat(count)}"
 }
@@ -35,15 +41,30 @@ fun stringToChuckNorris(input: String): String {
 private fun dechucknorissize(chucknorissized: String): String {
     val split = chucknorissized.split(" ")
     var binary = ""
-    for (i in split.indices step 2) {
-        val baseChar = if (split[i].length == 1) "1" else "0"
-        binary += baseChar.repeat(split[i + 1].length)
+    try {
+        for (i in split.indices step 2) {
+            val charSequenceLength = split[i].length
+            if (charSequenceLength > 2) {
+                throw ChuckNorrisEncodeException()
+            }
+            val baseChar = if (charSequenceLength == 1) "1" else "0"
+            binary += baseChar.repeat(split[i + 1].length)
+        }
+    } catch (e: IndexOutOfBoundsException) {
+        throw ChuckNorrisEncodeException()
     }
     return binary
 }
 
 fun chuckNorrisToString(input: String): String {
-    val binaries = dechucknorissize(input).chunked(7)
+    if (!input.matches("^[0 ]+$".toRegex())) {
+        throw ChuckNorrisEncodeException()
+    }
+    val decodedBinary = dechucknorissize(input)
+    if (decodedBinary.length % BINARY_SEQUENCE_LENGTH != 0) {
+        throw ChuckNorrisEncodeException()
+    }
+    val binaries = decodedBinary.chunked(BINARY_SEQUENCE_LENGTH)
     var result = ""
     for (binary in binaries) {
         val charCode = binaryToInt(binary)
